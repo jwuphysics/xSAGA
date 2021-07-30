97b93c13c69b65ec3ba722841e15b9bdc0f888e7
John Wu <jwuphysics@gmail.com>
2021-07-30 17:13:48 -0400

    evaluate based on p_CNN rather than surface density

--- xsaga/cnn_evaluation.py
+++ xsaga/cnn_evaluation.py
@@ -58,14 +58,15 @@ def get_top_metrics(df, N=None, p_CNN_threshold=None, z_cutoff=0.03):
             the `geometric_mean` of the purity and completeness.
     """
 
-    df['rank'] = df.p_CNN.rank(ascending=False)
+    if "ranking" not in df.columns:
+        df["ranking"] = df.p_CNN.rank(ascending=False)
 
     q_true = Query(f"Z < {z_cutoff}")
 
     if N is not None:
-        q_pred = Query(f"rank < {N}")
+        q_pred = Query(f"ranking < {N}")
     elif p_CNN_threshold is not None:
-        q_pred = Query(f"p_CNN > p_CNN_threshold")
+        q_pred = Query(f"p_CNN > {p_CNN_threshold}")
     else:
         raise ValueError("Either `N` or `p_CNN_threshold` must be provided.")
 
@@ -76,17 +77,22 @@ def get_top_metrics(df, N=None, p_CNN_threshold=None, z_cutoff=0.03):
 
     evaluation_metrics = {}
     evaluation_metrics["accuracy"] = (TP + TN) / (TP + TN + FP + FN)
-    evaluation_metrics["completeness"] = TP / (TP + FN)
-    evaluation_metrics["purity"] = TP / (TP + FP)
+    try:
+        evaluation_metrics["completeness"] = TP / (TP + FN)
+        evaluation_metrics["purity"] = TP / (TP + FP)
 
-    evaluation_metrics["geometric_mean"] = np.sqrt(
-        evaluation_metrics["completeness"] * evaluation_metrics["purity"]
-    )
+        evaluation_metrics["geometric_mean"] = np.sqrt(
+            evaluation_metrics["completeness"] * evaluation_metrics["purity"]
+        )
+    except ZeroDivisionError:
+        pass
 
     return evaluation_metrics
 
 
-def plot_metrics(df, surface_area, figname="CNN_metrics.png"):
+def plot_metrics(
+    df, label_surface_density=False, surface_area=None, figname="CNN_metrics.png"
+):
     """Plot evaluation metrics for cross-validated dataset that spans a given
     surface area. See `get_top_metrics()` for more details.
 
@@ -94,6 +100,8 @@ def plot_metrics(df, surface_area, figname="CNN_metrics.png"):
         df : pd.DataFrame
             DataFrame containing the ground-truth redshifts in column `Z` and
             low-z predictions in column `p_CNN`.
+        label_surface_density : bool
+            Whether to use the target surface density as the x-axis, or `p_CNN`.
         surface_area : float [deg^2]
             The area spanned by the data set, in units of square degrees.
         figname : str
@@ -103,14 +111,27 @@ def plot_metrics(df, surface_area, figname="CNN_metrics.png"):
 
     metrics_dict = {}
 
-    for surface_density in np.arange(1, 200, 1):
-        N = np.round(surface_density * surface_area).astype(int)
-        evaluation_metrics = get_top_metrics(df, N)
-        metrics_dict[surface_density] = evaluation_metrics
-
-    all_metrics_df = pd.DataFrame.from_dict(metrics_dict, orient="index").rename_axis(
-        "surface_density"
-    )
+    if label_surface_density:
+        for surface_density in np.arange(1, 200, 1):
+            if surface_area is None:
+                raise ValueError(
+                    """Supply either a surface area, or set `label_surface_density` \
+                    to False.
+                    """
+                )
+            N = np.round(surface_density * surface_area).astype(int)
+            evaluation_metrics = get_top_metrics(df, N=N)
+            metrics_dict[surface_density] = evaluation_metrics
+        all_metrics_df = pd.DataFrame.from_dict(
+            metrics_dict, orient="index"
+        ).rename_axis("surface_density")
+    else:
+        for p_CNN_threshold in np.arange(0, 1, 0.01):
+            evaluation_metrics = get_top_metrics(df, p_CNN_threshold=p_CNN_threshold)
+            metrics_dict[p_CNN_threshold] = evaluation_metrics
+        all_metrics_df = pd.DataFrame.from_dict(
+            metrics_dict, orient="index"
+        ).rename_axis("p_CNN_threshold")
 
     # plot
     fig, ax = plt.subplots(1, 1, figsize=(6, 4), dpi=300)
@@ -134,20 +155,24 @@ def plot_metrics(df, surface_area, figname="CNN_metrics.png"):
         label="Geometric mean",
     )
 
-    surface_densities = [1, 2, 3, 5, 10, 15, 20, 30, 50, 100]
-    ax.set_xscale("log")
-    ax.set_xticks(surface_densities)
-    ax.set_xticklabels(surface_densities)
+    if label_surface_density:
+        ax.set_xlabel("Surface density [deg$^{-2}$]", fontsize=12)
+        surface_densities = [1, 2, 3, 5, 10, 15, 20, 30, 50, 100]
+        ax.set_xscale("log")
+        ax.set_xticks(surface_densities)
+        ax.set_xticklabels(surface_densities)
+    else:
+        ax.set_xlabel(r"$p_{\rm CNN}$", fontsize=12)
+        ax.set_xscale("linear")
 
     ax.set_ylim(0, 1)
-    ax.set_xlabel("Surface density [deg$^{-2}$]", fontsize=12)
     ax.set_ylabel("Evaluation metric")
 
     ax.grid(alpha=0.15)
     ax.legend(framealpha=0, loc="lower center")
 
     fig.tight_layout()
-    fig.savefig(results_dir / f"plots/{figname}")
+    fig.savefig(results_dir / f"plots/cnn-evaluation/{figname}")
 
 
 def compare_north_and_south():
@@ -177,8 +202,27 @@ if __name__ == "__main__":
     # load SAGA crossvalidation results
     # =================================
     saga_cv = load_saga_crossvalidation()
+    saga = pd.read_csv(
+        ROOT / "data/saga_redshifts_2021-02-19.csv", dtype={"OBJID": str}
+    )
+    saga_cv = saga_cv.merge(saga, on="OBJID")
 
-    N_hosts = 89
-    saga_area = N_hosts * np.pi * 1 ** 2
+    saga_cv["ranking"] = saga_cv.p_CNN.rank(ascending=False)
 
-    plot_metrics(saga_cv, saga_area)
+    # N_hosts = 89
+    # saga_area = N_hosts * np.pi * 1 ** 2
+    # plot_metrics(saga_cv, label_surface_density=True, surface_area=saga_area)
+
+    plot_metrics(saga_cv, label_surface_density=False, figname="saga_all.png")
+
+    plot_metrics(
+        saga_cv[saga_cv.r_mag < 17.78],
+        label_surface_density=False,
+        figname="saga_r-below-17p78.png",
+    )
+
+    plot_metrics(
+        saga_cv[saga_cv.r_mag > 17.78],
+        label_surface_density=False,
+        figname="saga_r-above-17p78.png",
+    )
