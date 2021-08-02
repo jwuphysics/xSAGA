7c3406d21ec3ef66f526b73d1fa9634a6a65b99a
John Wu <jwuphysics@gmail.com>
2021-08-02 16:31:56 -0400

    fix bugs

--- xsaga/cnn_train.py
+++ xsaga/cnn_train.py
@@ -11,7 +11,7 @@ import pandas as pd
 import torch
 
 from fastai2.basics import *
-from fastai2.vision import *
+from fastai2.vision.all import *
 from mish_cuda import MishCuda
 from model.xresnet_deconv import xresnet34_hybrid
 from pathlib import Path
@@ -21,6 +21,7 @@ from tqdm import tqdm
 
 seed = 42
 ImageFile.LOAD_TRUNCATED_IMAGES = True
+MIN_JPG_SIZE = 4096
 
 ROOT = Path(__file__).resolve().parent.parent
 results_dir = ROOT / "results/xSAGA"
@@ -36,7 +37,7 @@ batch_tfms = aug_transforms(
     max_zoom=1.0, flip_vert=True, max_lighting=0.0, max_warp=0.0
 ) + [Normalize.from_stats(*legacy_image_stats)]
 
-learner_metrics = [accuracy, F1Score(), Recall(), Precision(), RocAucBinary()]
+learner_metrics = [accuracy, F1Score(), Recall(), Precision(), RocAuc()]
 
 dtype_dict = {
     "OBJID": str,
@@ -77,30 +78,6 @@ class FocalLossFlat(CrossEntropyLossFlat):
         )
 
 
-def load_learner(bs):
-    """Return Fastai Learner object from DataFrame assuming a hybrid-deconv xresnet34
-    model with Focal Loss and a ranger optimizer.
-    """
-
-    dblock = DataBlock(
-        blocks=(ImageBlock, CategoryBlock),
-        get_x=ColReader(["OBJID"], pref=f"{ROOT}/{img_dir}/", suff=".jpg"),
-        get_y=ColReader(label_col),
-        splitter=ColSplitter("kfold_split"),
-        item_tfms=item_tfms,
-        batch_tfms=batch_tfms,
-    )
-
-    dls = ImageDataLoaders.from_dblock(dblock, df, path=ROOT, bs=bs)
-
-    loss_func = FocalLossFlat(gamma=2)
-    model = xresnet34_hybrid(n_out=2, sa=True, act_cls=MishCuda, groups=64, reduction=8)
-
-    learn = Learner(dls, model, opt_func=ranger, loss_func=loss_func)
-
-    return learn
-
-
 def train(
     df,
     K=4,
@@ -108,13 +85,15 @@ def train(
     n_epochs=10,
     max_lr=1e-2,
     img_dir="images-legacy_saga-2021-02-19",
-    label_col="low_z",
-    version="2019-02-19",
+    version="2021-02-19",
 ):
     """Train the CNN using k-fold cross-validation.
     """
 
-    kf = KFold(K, random_state=seed)
+    df = df.sample(frac=1, random_state=seed).copy()
+    df["low_z"] = df.SPEC_Z < 0.03
+
+    kf = KFold(K)
     df["kfold"] = -1
 
     N = len(df)
@@ -122,17 +101,35 @@ def train(
 
     for k, [_, val_idx] in enumerate(kf.split(range(N))):
 
-        df["kfold"][val_idx] = k
+        k = k + 1
+        df.loc[val_idx, "kfold"] = k
         df["kfold_split"] = df.kfold == k
 
-        learn = load_learner(df, bs=bs, version=version)
+        # load data
+        dblock = DataBlock(
+            blocks=(ImageBlock, CategoryBlock),
+            get_x=ColReader(["OBJID"], pref=f"{ROOT}/{img_dir}/", suff=".jpg"),
+            get_y=ColReader("low_z"),
+            splitter=ColSplitter("kfold_split"),
+            item_tfms=item_tfms,
+            batch_tfms=batch_tfms,
+        )
+
+        dls = ImageDataLoaders.from_dblock(dblock, df, path=ROOT, bs=bs)
+
+        loss_func = FocalLossFlat(gamma=2)
+        model = xresnet34_hybrid(
+            n_out=2, sa=True, act_cls=MishCuda, groups=64, reduction=8
+        )
+
+        learn = Learner(dls, model, opt_func=ranger, loss_func=loss_func)
 
         # train
         learn.fit_one_cycle(n_epochs, max_lr)
 
         torch.save(
             learn.model.state_dict(),
-            ROOT / f"models/saga_FL-hdxresnet34-sz{sz}_{version}_{k}-of-{K}",
+            ROOT / f"models/saga_FL-hdxresnet34-sz{sz}_{version}_{k}-of-{K}.pth",
         )
 
         # get cross-validation predictions
@@ -151,39 +148,49 @@ def train(
 
 def predict(
     df,
+    filenames,
     K=4,
+    img_dir="images-legacy_saga-2021-02-19",
     bs=128,
-    pred_img_dir="images-legacy-dr9",
-    MIN_JPG_SIZE=4096,
     version="2019-02-19",
 ):
     """Make predictions using model trained above
     """
 
-    kf = KFold(K, random_state=seed)
-    N = len(df)
+    model = xresnet34_hybrid(n_out=2, sa=True, act_cls=MishCuda, groups=64, reduction=8)
 
-    filenames = list(
-        x
-        for x in (ROOT / "{pred_img_dir}").rglob("*.jpg")
-        if (x.stat().st_size >= MIN_JPG_SIZE)
+    all_preds = []
+
+    # load dls in order to do the same transforms
+    df["low_z"] = df.SPEC_Z < 0.03
+
+    dblock = DataBlock(
+        blocks=(ImageBlock, CategoryBlock),
+        get_x=ColReader(["OBJID"], pref=f"{ROOT}/{img_dir}/", suff=".jpg"),
+        get_y=ColReader("low_z"),
+        splitter=RandomSplitter(0),
+        item_tfms=item_tfms,
+        batch_tfms=batch_tfms,
     )
 
-    for k, [_, val_idx] in enumerate(kf.split(range(N))):
+    dls = ImageDataLoaders.from_dblock(dblock, df, path=ROOT, bs=bs)
+    test_dl = dls.test_dl(filenames, num_workers=8, bs=bs)
 
-        df["kfold"][val_idx] = k
-        df["kfold_split"] = df.kfold == k
+    for k in range(K):
+        k = k + 1
 
-        learn = load_learner(df, bs=bs)
-        learn.load(ROOT / f"models/saga_FL-hdxresnet34-sz{sz}_{version}_{k}-of-{K}")
-
-        test_dl = learn.dls.test_dl(filenames, num_workers=8, bs=32)
+        model.load_state_dict(
+            torch.load(ROOT / f"models/saga_FL-hdxresnet34-sz{sz}_{version}_{k}-of-4")
+        )
+        model.to("cuda")
 
         # evaluate
-        m = learn.model.eval()
+        print(f"Loaded model {k}")
+
+        m = model.eval()
         outputs = []
         with torch.no_grad():
-            for (xb,) in tqdm.tqdm(iter(test_dl), total=len(test_dl)):
+            for (xb,) in tqdm(iter(test_dl), total=len(test_dl)):
                 outputs.append(m(xb).cpu())
 
         outs = torch.cat(outputs)
@@ -196,9 +203,23 @@ def predict(
         preds = pd.DataFrame({f"p_CNN_{k}": p_CNNs[:, 1]}, index=objIDs)
         preds.to_parquet(results_dir / f"predictions-{k}.parquet")
 
-    pass
+        all_preds.append(preds)
+
+    all_preds = pd.concat(all_preds)
+    all_preds.drop("kfold_split", axis=1, inplace=True)
+
+    return all_preds
 
 
 if __name__ == "__main__":
     saga = pd.read_csv(ROOT / "data/saga_redshifts_2021-02-19.csv", dtype=dtype_dict)
     train(saga, n_epochs=1)
+
+    # predict
+    filenames = list(
+        x
+        for x in (ROOT / "images-legacy-dr9").rglob("*.jpg")
+        if (x.stat().st_size >= MIN_JPG_SIZE)
+    )
+
+    all_preds = predict(saga, filenames)
